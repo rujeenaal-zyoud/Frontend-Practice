7f86737ab84deb4066cd671c768b0c2d38274a45
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-06-11 18:57:25 +0300

    3

---
+++ frontend/src/components/FavoriteRecipes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FavoriteRecipes extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FavoriteRecipes
